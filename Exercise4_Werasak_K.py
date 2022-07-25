188d5b6ed84f55e5cf42dc32942df79b6b202ed2
Mickwerasak <109936860+Mickwerasak@users.noreply.github.com>
2022-07-25 11:43:23 +0700

    Add file Exercise4_Werasak_K.py

---
+++ Exercise4_Werasak_K.py
@@ -0,0 +1,9 @@
+FB = 10
+GB = 9.5
+ICS = 8
+CP = 9.5
+print("---Your Score---")
+print("Foundation English           :", FB)
+print("General Business             :", GB)
+print("Introtion to Computer System :", ICS)
+print("Computer Programming         :", CP)
